0db81fc4520d15b66bee6e1a5ba2368000f6f650
LE DEZ Pierre <74079020+PierreLEDEZ@users.noreply.github.com>
2021-04-21 16:43:31 +0200

    Delete bvh2rgb function and add comments

--- src/predicter.py
+++ src/predicter.py
@@ -9,7 +9,18 @@ from .bvh import bvh_parser
 import lib.bvh_transformation as bvh_transformation
 
 class Predicter:
+    """
+        Class in charge of converting frames in image(s) and make predictions.
+    """
+
     def __init__(self):
+        """
+            Initialize the predicter
+
+            Load the pretrained model, parse an example bvh file to obtain the skeleton
+            Get the joints list and the joints list of ignored joints
+        """
+
         self.frames = []
         self.predictions = []
         self.model = models.load_model("./model/parallel_resnet50_mkI_xy_xz_yz_4classes_without_P01.h5")
@@ -21,50 +32,31 @@ class Predicter:
         self.ignored_joints_index = utils.ignoreJoints(self.bvhParser, "geo", utils.IGNORED_JOINTS)
     
     def update_frames(self, frames):
+        """
+            Save the received frames in a numpy array
+
+            :param frames: received frames from the client thread
+            :type frames: list
+        """
+
         self.frames = np.array(frames, dtype=np.float32)
 
-    def bvh2RGB(self):
-        width, height = len(self.frames), len(self.joints)*2
-        img_struct = (height, width, 3)
-
-        img_content = np.zeros(img_struct, dtype=np.uint8)
-        
-        translation_min_max_X, translation_min_max_Y, translation_min_max_Z = utils.get_min_max_translation(self.frames)
-        rotation_min_max_X, rotation_min_max_Y, rotation_min_max_Z = utils.get_min_max_rotation(self.frames)
-
-        for nb_frame, frame in enumerate(self.frames):
-            joint_placed = 0
-            for nb_joint in range(0, height):
-                if nb_joint in self.ignored_joints_index:
-                    joint_placed += 3
-                    continue
-                if nb_joint % 2 == 0:
-                    img_content[nb_joint][nb_frame] = utils.calculate_laraba_value_translation(frame, joint_placed, translation_min_max_X, translation_min_max_Y, translation_min_max_Z)
-                else:
-                    img_content[nb_joint][nb_frame] = utils.calculate_laraba_value_rotation(frame, joint_placed, rotation_min_max_X, rotation_min_max_Y, rotation_min_max_Z)
-
-                joint_placed += 3
-        img_content = np.delete(img_content, self.ignored_joints_index, axis=0)
-
-        R,G,B = cv2.split(img_content)
-        img_content = cv2.merge([B,G,R])
-
-        final_img = cv2.resize(img_content, (236,118), interpolation=cv2.INTER_NEAREST)
-
-        final_img = cv2.cvtColor(final_img, cv2.COLOR_BGR2HSV)
-        H,S,V = cv2.split(final_img)
-        ahe = cv2.createCLAHE(clipLimit=CLIPLIMIT, tileGridSize=(8,8))
-        V = ahe.apply(V)
-        final_img = cv2.merge([H,S,V])
-        final_img = cv2.cvtColor(final_img, cv2.COLOR_HSV2BGR)
-        return final_img
-
     def run(self):
+        """
+            Main function of this class
+
+            Create a thread and start it
+        """
+
         t = threading.Thread(target=self.predict_classes)
         t.daemon=True
         t.start()
 
     def predict_classes(self):
+        """
+            Convert received frames in images with the specified method and make predictions
+        """
+
         # final_img = bvh_transformation.bvh2GeometricFeaturesV2(self.frames, self.joints, utils.IGNORED_JOINTS, self.ignored_joints_index)
         # final_img = tensorflow.keras.applications.resnet.preprocess_input(final_img)
         
